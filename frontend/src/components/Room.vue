83f27ef35d902197fdeec95ff07d24b3feec1ce2
John Smith <xfgryujk@126.com>
2019-05-26 18:33:08 +0800

    修复字体

--- frontend/src/components/Room.vue
+++ frontend/src/components/Room.vue
@@ -332,9 +332,9 @@ yt-live-chat-ticker-paid-message-item-renderer #fake-avatar {
 
 /* 以下为自动生成：https://chatv2.septapus.com/ */
 
-/* @import url("https://fonts.googleapis.com/css?family=Changa One"); */
+/* @import url("https://fonts.googleapis.com/css?family=Changa%20One"); */
 /* @import url("https://fonts.googleapis.com/css?family=Imprima"); */
-@import url("https://fonts.lug.ustc.edu.cn/css?family=Changa One");
+@import url("https://fonts.lug.ustc.edu.cn/css?family=Changa%20One");
 @import url("https://fonts.lug.ustc.edu.cn/css?family=Imprima");
 
 /* Background colors*/
