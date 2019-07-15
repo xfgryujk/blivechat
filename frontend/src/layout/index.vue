fc77b246c2431356a97ef1585316acf54cacf6ac
John Smith <xfgryujk@126.com>
2019-07-15 18:01:38 +0800

    我傻了

--- frontend/src/layout/index.vue
+++ frontend/src/layout/index.vue
@@ -122,9 +122,3 @@ a, a:focus, a:hover {
   display: none;
 }
 </style>
-
-<style scoped>
-* {
-  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
-}
-</style>
