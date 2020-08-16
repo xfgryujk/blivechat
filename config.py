127344802d4b0a2ec995c71dc736f0929e2f829b
John Smith <xfgryujk@126.com>
2020-08-16 12:13:53 +0800

    添加允许自动翻译的房间配置

--- config.py
+++ config.py
@@ -13,14 +13,20 @@ _config: Optional['AppConfig'] = None
 
 
 def init():
-    reload()
+    if reload():
+        return
+    logger.warning('Using default config')
+    global _config
+    _config = AppConfig()
 
 
 def reload():
     config = AppConfig()
-    if config.load(CONFIG_PATH):
-        global _config
-        _config = config
+    if not config.load(CONFIG_PATH):
+        return False
+    global _config
+    _config = config
+    return True
 
 
 def get_config():
@@ -31,6 +37,7 @@ class AppConfig:
     def __init__(self):
         self.database_url = 'sqlite:///data/database.db'
         self.enable_translate = True
+        self.allow_translate_rooms = {}
 
     def load(self, path):
         config = configparser.ConfigParser()
@@ -39,6 +46,12 @@ class AppConfig:
             app_section = config['app']
             self.database_url = app_section['database_url']
             self.enable_translate = app_section.getboolean('enable_translate')
+            allow_translate_rooms = app_section['allow_translate_rooms'].strip()
+            if allow_translate_rooms == '':
+                self.allow_translate_rooms = {}
+            else:
+                allow_translate_rooms = allow_translate_rooms.split(',')
+                self.allow_translate_rooms = set(map(lambda id_: int(id_.strip()), allow_translate_rooms))
         except (KeyError, ValueError):
             logger.exception('Failed to load config:')
             return False
