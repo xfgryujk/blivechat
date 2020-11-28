098bc86e0c30388c5ee3b0bc437f8f906fb3f65f
John Smith <xfgryujk@126.com>
2020-11-28 22:57:30 +0800

    更新版本号v1.5.1

--- frontend/src/layout/index.vue
+++ frontend/src/layout/index.vue
@@ -9,7 +9,7 @@
         </router-link>
       </div>
       <div class="version">
-        v1.5.0
+        v1.5.1
       </div>
       <sidebar></sidebar>
     </el-aside>
--- update.py
+++ update.py
@@ -4,7 +4,7 @@ import asyncio
 
 import aiohttp
 
-VERSION = 'v1.5.0'
+VERSION = 'v1.5.1'
 
 
 def check_update():
