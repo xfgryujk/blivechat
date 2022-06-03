1075b458aaa4ac97cd1463bd9ec26c750c3c8f47
John Smith <xfgryujk@126.com>
2022-06-03 13:47:22 +0800

    数据库中uid类型改成BigInteger

--- models/bilibili.py
+++ models/bilibili.py
@@ -6,6 +6,6 @@ import models.database
 
 class BilibiliUser(models.database.OrmBase):
     __tablename__ = 'bilibili_users'
-    uid = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    uid = sqlalchemy.Column(sqlalchemy.BigInteger, primary_key=True)  # 创建表后最好手动改成unsigned
     avatar_url = sqlalchemy.Column(sqlalchemy.String(100))
     update_time = sqlalchemy.Column(sqlalchemy.DateTime)
