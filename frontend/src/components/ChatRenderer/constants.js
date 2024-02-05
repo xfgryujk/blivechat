7343cc0a86861d8c0a4107aa7554bffffe5ee13b
John Smith <xfgryujk@126.com>
2024-02-05 23:26:58 +0800

    优化查找礼物配置的算法

--- frontend/src/components/ChatRenderer/constants.js
+++ frontend/src/components/ChatRenderer/constants.js
@@ -39,106 +39,8 @@ export const CONTENT_TYPE_IMAGE = 1
 
 // 美元 -> 人民币 汇率
 const EXCHANGE_RATE = 7
-export const PRICE_CONFIGS = [
-  // $100红
-  {
-    price: 100 * EXCHANGE_RATE,
-    colors: {
-      contentBg: 'rgba(230,33,23,1)',
-      headerBg: 'rgba(208,0,0,1)',
-      header: 'rgba(255,255,255,1)',
-      authorName: 'rgba(255,255,255,0.701961)',
-      time: 'rgba(255,255,255,0.501961)',
-      content: 'rgba(255,255,255,1)'
-    },
-    pinTime: 60,
-    priceLevel: 7,
-  },
-  // $50品红
-  {
-    price: 50 * EXCHANGE_RATE,
-    colors: {
-      contentBg: 'rgba(233,30,99,1)',
-      headerBg: 'rgba(194,24,91,1)',
-      header: 'rgba(255,255,255,1)',
-      authorName: 'rgba(255,255,255,0.701961)',
-      time: 'rgba(255,255,255,0.501961)',
-      content: 'rgba(255,255,255,1)'
-    },
-    pinTime: 30,
-    priceLevel: 6,
-  },
-  // $20橙
-  {
-    price: 20 * EXCHANGE_RATE,
-    colors: {
-      contentBg: 'rgba(245,124,0,1)',
-      headerBg: 'rgba(230,81,0,1)',
-      header: 'rgba(255,255,255,0.87451)',
-      authorName: 'rgba(255,255,255,0.701961)',
-      time: 'rgba(255,255,255,0.501961)',
-      content: 'rgba(255,255,255,0.87451)'
-    },
-    pinTime: 10,
-    priceLevel: 5,
-  },
-  // $10黄
-  {
-    price: 10 * EXCHANGE_RATE,
-    colors: {
-      contentBg: 'rgba(255,202,40,1)',
-      headerBg: 'rgba(255,179,0,1)',
-      header: 'rgba(0,0,0,0.87451)',
-      authorName: 'rgba(0,0,0,0.541176)',
-      time: 'rgba(0,0,0,0.501961)',
-      content: 'rgba(0,0,0,0.87451)'
-    },
-    pinTime: 5,
-    priceLevel: 4,
-  },
-  // $5绿
-  {
-    price: 5 * EXCHANGE_RATE,
-    colors: {
-      contentBg: 'rgba(29,233,182,1)',
-      headerBg: 'rgba(0,191,165,1)',
-      header: 'rgba(0,0,0,1)',
-      authorName: 'rgba(0,0,0,0.541176)',
-      time: 'rgba(0,0,0,0.501961)',
-      content: 'rgba(0,0,0,1)'
-    },
-    pinTime: 2,
-    priceLevel: 3,
-  },
-  // $2浅蓝
-  {
-    price: 2 * EXCHANGE_RATE,
-    colors: {
-      contentBg: 'rgba(0,229,255,1)',
-      headerBg: 'rgba(0,184,212,1)',
-      header: 'rgba(0,0,0,1)',
-      authorName: 'rgba(0,0,0,0.701961)',
-      time: 'rgba(0,0,0,0.501961)',
-      content: 'rgba(0,0,0,1)'
-    },
-    pinTime: 0,
-    priceLevel: 2,
-  },
-  // $1蓝
-  {
-    price: EXCHANGE_RATE,
-    colors: {
-      contentBg: 'rgba(30,136,229,1)',
-      headerBg: 'rgba(21,101,192,1)',
-      header: 'rgba(255,255,255,1)',
-      authorName: 'rgba(255,255,255,0.701961)',
-      time: 'rgba(255,255,255,0.501961)',
-      content: 'rgba(255,255,255,1)'
-    },
-    pinTime: 0,
-    priceLevel: 1,
-  },
-  // $0淡蓝
+const PRICE_CONFIGS = [
+  // 0 淡蓝
   {
     price: 0,
     colors: {
@@ -152,15 +54,116 @@ export const PRICE_CONFIGS = [
     pinTime: 0,
     priceLevel: 0,
   },
+  // ¥0.01 蓝
+  {
+    price: 0.01,
+    colors: {
+      contentBg: 'rgba(30,136,229,1)',
+      headerBg: 'rgba(21,101,192,1)',
+      header: 'rgba(255,255,255,1)',
+      authorName: 'rgba(255,255,255,0.701961)',
+      time: 'rgba(255,255,255,0.501961)',
+      content: 'rgba(255,255,255,1)'
+    },
+    pinTime: 0,
+    priceLevel: 1,
+  },
+  // $2 浅蓝
+  {
+    price: 2 * EXCHANGE_RATE,
+    colors: {
+      contentBg: 'rgba(0,229,255,1)',
+      headerBg: 'rgba(0,184,212,1)',
+      header: 'rgba(0,0,0,1)',
+      authorName: 'rgba(0,0,0,0.701961)',
+      time: 'rgba(0,0,0,0.501961)',
+      content: 'rgba(0,0,0,1)'
+    },
+    pinTime: 0,
+    priceLevel: 2,
+  },
+  // $5 绿
+  {
+    price: 5 * EXCHANGE_RATE,
+    colors: {
+      contentBg: 'rgba(29,233,182,1)',
+      headerBg: 'rgba(0,191,165,1)',
+      header: 'rgba(0,0,0,1)',
+      authorName: 'rgba(0,0,0,0.541176)',
+      time: 'rgba(0,0,0,0.501961)',
+      content: 'rgba(0,0,0,1)'
+    },
+    pinTime: 2,
+    priceLevel: 3,
+  },
+  // $10 黄
+  {
+    price: 10 * EXCHANGE_RATE,
+    colors: {
+      contentBg: 'rgba(255,202,40,1)',
+      headerBg: 'rgba(255,179,0,1)',
+      header: 'rgba(0,0,0,0.87451)',
+      authorName: 'rgba(0,0,0,0.541176)',
+      time: 'rgba(0,0,0,0.501961)',
+      content: 'rgba(0,0,0,0.87451)'
+    },
+    pinTime: 5,
+    priceLevel: 4,
+  },
+  // $20 橙
+  {
+    price: 20 * EXCHANGE_RATE,
+    colors: {
+      contentBg: 'rgba(245,124,0,1)',
+      headerBg: 'rgba(230,81,0,1)',
+      header: 'rgba(255,255,255,0.87451)',
+      authorName: 'rgba(255,255,255,0.701961)',
+      time: 'rgba(255,255,255,0.501961)',
+      content: 'rgba(255,255,255,0.87451)'
+    },
+    pinTime: 10,
+    priceLevel: 5,
+  },
+  // $50 品红
+  {
+    price: 50 * EXCHANGE_RATE,
+    colors: {
+      contentBg: 'rgba(233,30,99,1)',
+      headerBg: 'rgba(194,24,91,1)',
+      header: 'rgba(255,255,255,1)',
+      authorName: 'rgba(255,255,255,0.701961)',
+      time: 'rgba(255,255,255,0.501961)',
+      content: 'rgba(255,255,255,1)'
+    },
+    pinTime: 30,
+    priceLevel: 6,
+  },
+  // $100 红
+  {
+    price: 100 * EXCHANGE_RATE,
+    colors: {
+      contentBg: 'rgba(230,33,23,1)',
+      headerBg: 'rgba(208,0,0,1)',
+      header: 'rgba(255,255,255,1)',
+      authorName: 'rgba(255,255,255,0.701961)',
+      time: 'rgba(255,255,255,0.501961)',
+      content: 'rgba(255,255,255,1)'
+    },
+    pinTime: 60,
+    priceLevel: 7,
+  },
 ]
 
 export function getPriceConfig(price) {
-  for (const config of PRICE_CONFIGS) {
-    if (price >= config.price) {
-      return config
+  let i = 0
+  // 根据先验知识，从小找到大通常更快结束
+  for (; i < PRICE_CONFIGS.length - 1; i++) {
+    let nextConfig = PRICE_CONFIGS[i + 1]
+    if (price < nextConfig.price) {
+      return PRICE_CONFIGS[i]
     }
   }
-  return PRICE_CONFIGS[PRICE_CONFIGS.length - 1]
+  return PRICE_CONFIGS[i]
 }
 
 export function getShowContent(message) {
